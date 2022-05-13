6ef8df0d4479dbcbe2a3c2dc94a5a26d2b374cf0
Josh Elder <joshelder0123@gmail.com>
2022-05-13 16:39:19 +0100

    API 1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const port = process.env.port || 3000
-app.get('/API', (req, res) =>{
+app.get('/', (req, res) =>{
     const apiKey = req.query.apiKey;
     res.send({data:'TEST'});
 });
